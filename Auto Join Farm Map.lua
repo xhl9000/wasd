ea0e158d6ae5da2b90a86f48fcd8f8e540f97d1b
xhl9000 <87189522+xhl9000@users.noreply.github.com>
2021-07-09 21:12:11 +0800

    Update Auto Join Farm Map.lua

--- Auto Join Farm Map.lua
+++ Auto Join Farm Map.lua
@@ -1,9 +1,10 @@
-plr = game.Players.LocalPlayer
-hum = plr.Character.HumanoidRootPart
+
 while not game:IsLoaded() do
   wait(10)
 end
 wait (5)
+plr = game.Players.LocalPlayer
+hum = plr.Character.HumanoidRootPart
 hum.CFrame = CFrame.new(-971.146057, 59.9670563, -903.007751, -0.707134247, 0, -0.707079291, 0, 1, 0, 0.707079291, 0, -0.707134247)
 wait(0.5)
 hum.CFrame = CFrame.new(-686.755371, 49.1979294, -619.110535, -0.996667266, -9.27441768e-09, -0.0815740228, -6.33455999e-09, 1, -3.62979407e-08, 0.0815740228, -3.56602357e-08, -0.996667266)
